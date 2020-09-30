32b705bf397620e91a05af20e9bbaae1c5713c90
Airas <airastolonov05@gmail.com>
2020-09-30 17:27:18 +0600

    Слелал игру Камень Ножницы Бумага

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [state,setState] = useState({
+    bot:[
+        {string: 'камень',lose: 'бумага',win:'ножницы'},
+        {string: 'бумага',lose: 'ножницы',win:'камень'},
+        {string: 'ножницы',lose: 'камень',win:'бумага'}
+    ],
+    player:'',
+    botNum:0,
+    playerNum: 0,
+  });
+
+  const playHandler = () => {
+    if(state.player === 'ножницы'||state.player === 'бумага'||state.player === 'камень'){
+      const bot = Math.floor(Math.random() * state.bot.length);
+      if(state.player === state.bot[bot].string){
+        alert('Ничья')
+      }else if(state.player === state.bot[bot].win){
+        setState({...state, playerNum: state.playerNum + 1});
+        alert(` Игрок: ${state.bot[bot].win} \n Бот: ${state.bot[bot].lose} \n Игрок + 1 бал`);
+        if(state.playerNum > 4){
+          alert(`Игрок выиграл!! \n счета обнуляются`);
+          setState({...state, playerNum: 0, botNum: 0})
+        }
+      }else{
+        setState({...state, botNum: state.botNum + 1});
+        alert(`Игрок: ${state.bot[bot].lose} \n Бот: ${state.bot[bot].win} \n Бот + 1 бал`);
+        if(state.botNum > 4){
+          alert(`Бот выиграл!! \n счета обнуляются`);
+          setState({...state, playerNum: 0, botNum: 0})
+        }
+      }
+    }else {
+      alert('Введите корректный вариант!')
+    }
+  };
+
+  const inputValHandler = (e) => {
+    setState({...state,[e.target.name]: e.target.value});
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Варианты:</p>
+      <p><i>камень</i> <br/> <i>ножницы</i> <br/> <i>бумага</i></p>
+      <p>Введите ваш вариант:</p>
+      <input type="text" name="player" onChange={inputValHandler}/>
+      <button onClick={playHandler}>бой!</button>
+      <p>Игрок: {state.playerNum}</p>
+      <p>Бот: {state.botNum}</p>
     </div>
   );
 }
